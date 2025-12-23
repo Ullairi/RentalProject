5bc57284047e25642f7a0301ae9bc83df41abe8f
Ullairi <maks.tih2005@gmail.com>
2025-12-23 20:33:34 +0100

    feat: add starting Listing model for Favorite model relation from users/models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.mixins import TimestampMixin
 
-# Create your models here.
+
+class Listing(TimestampMixin):
+    """Listing model for Favorite model from users/models relation"""
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'listings'
+
+    def __str__(self):
+        return self.title
