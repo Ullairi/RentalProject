d705e6e098d05b89f7cd3136aab0fad16df55d7a
Ullairi <maks.tih2005@gmail.com>
2025-12-25 21:58:38 +0100

    feat: add Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from core.mixins import TimestampMixin
+from core.validators import validate_rating
 
-# Create your models here.
+
+class Review(TimestampMixin):
+    """Review model for listings"""
+    listing = models.ForeignKey(
+        'listings.Listing',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    rating = models.PositiveIntegerField(validators=[validate_rating])
+    comment = models.TextField()
+
+    class Meta:
+        db_table = 'reviews'
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+        ordering = ['-created_at']
+        unique_together = ('listing', 'author')
+
+    def __str__(self):
+        return f'Review by {self.author.username} for {self.listing.title}'
